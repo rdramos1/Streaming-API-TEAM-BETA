32f55d05f6ee4e6769fe07c98329654ee6e141af
gabizinha12 <gaibou_lima@hotmail.com>
2025-10-24 02:23:34 -0300

    feature: implement method

--- src/main/java/com/studynow/StreamingAPI/entities/Movie.java
+++ src/main/java/com/studynow/StreamingAPI/entities/Movie.java
@@ -1,8 +1,13 @@
 package com.studynow.StreamingAPI.entities;
 
+import com.studynow.StreamingAPI.averagerating.IAverageRating;
 import com.studynow.StreamingAPI.enums.Genre;
 import com.studynow.StreamingAPI.enums.Rating;
 
-public record Movie(Long id, String title, String description, int releaseYear, Genre genre, Rating rating,int duration, String director, String videoUrl) {
+public record Movie(Long id, String title, String description, int releaseYear, Genre genre, Rating rating,int duration, String director, String videoUrl) implements IAverageRating {
 
+    @Override
+    public double calculateAverageRating() {
+        return 0;
+    }
 }
